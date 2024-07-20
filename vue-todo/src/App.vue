74ba20102c5aceb26eb76c69718e8bc96bfd3a02
Brandon Wie <brandonwie.cs@gmail.com>
2024-07-20 18:24:37 +0900

    Use Map to remove target later

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -6,15 +6,16 @@ import { onMounted } from "vue";
 const inputRef = ref(null);
 
 const data = reactive({
-  todos: ["first item"],
+  todos: new Map([[0, "Learn Vue 3"]]),
 });
 
 const handleAddTodo = () => {
-  const value = inputRef.value.value.trim();
+  const value = inputRef.value?.value.trim(); // Optional chaining to prevent null reference
   if (value) {
-    data.todos.push(value);
-    inputRef.value.value = "";
-    inputRef.value.focus();
+    const id = data.todos.size + 1; // Generate a new id
+    data.todos.set(id, value);
+    inputRef.value.value = ""; // Clear the input field
+    inputRef.value.focus(); // Refocus the input field
   }
 };
 
@@ -28,7 +29,7 @@ onMounted(() => {
     <h1>Todos</h1>
     <input type="text" ref="inputRef" @keyup.enter="handleAddTodo" />
     <ul>
-      <TodoItem v-for="(todo, index) in data.todos" :key="index" :todo="todo" />
+      <TodoItem v-for="[id, todo] in data.todos" :key="id" :todo="todo" />
     </ul>
     <button @click="handleAddTodo">Add Todo</button>
   </section>
